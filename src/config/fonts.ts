825a874dbb7c5bc04471aaa1e4b345eb3fed9f3e
Fernando Parrales <tuemail@gmail.com>
2026-02-23 20:28:20 -0600

    fix: prisma generate on vercel build2345

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,6 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Inter, Montserrat_Alternates } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-})
+});
+
+export const titleFont = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
